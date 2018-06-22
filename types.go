bc39137a2af5b2cab8218e5589ce26e1196e06d4
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-06-23 01:05:42 +0200

    feat: add basic types to parse sonarr responses

---
+++ types.go
@@ -0,0 +1,49 @@
+package main
+
+const (
+	// EnvSonarrURL ...
+	EnvSonarrURL = "SONARR_URL"
+	// EnvSonarrAPIKey ...
+	EnvSonarrAPIKey = "SONARR_APIKEY"
+	// EnvSonarrDownloadFolder ...
+	EnvSonarrDownloadFolder = "SONARR_DOWNLOAD_FOLDER"
+	// StatusWarning ...
+	StatusWarning = "Warning"
+)
+
+// QueueElement ...
+type QueueElement struct {
+	Title                 string
+	Status                string
+	Episode               Episode
+	Series                Series
+	TrackedDownloadStatus string
+}
+
+// Episode ...
+type Episode struct {
+	SeasonNumber  int
+	EpisodeNumber int
+}
+
+// Series ...
+type Series struct {
+	Title string
+}
+
+// EpisodeQuality ...
+type EpisodeQuality struct {
+	Name    string
+	Quality Quality
+}
+
+// Quality ...
+type Quality struct {
+	Name string
+}
+
+// Command ...
+type Command struct {
+	Name string
+	Path string
+}
