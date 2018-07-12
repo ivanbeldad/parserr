d73b493ca0fc614eeed25e27220e7b4c24cc09b7
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-07-12 02:53:26 +0200

    feat(core): add radarr to main check

--- main.go
+++ main.go
@@ -5,60 +5,70 @@ import (
 	"os"
 	"sonarr-parser-helper/api"
 	"sonarr-parser-helper/parser"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	loadEnvOrFatal()
-	// check()
-	testRadarr()
+	apis := getAPIs()
+	for _, a := range apis {
+		execute(a)
+	}
 }
 
-func createAPI() api.API {
-	return api.NewAPI(
-		os.Getenv("RADARR_URL"),
-		os.Getenv("RADARR_APIKEY"),
-		os.Getenv("RADARR_DOWNLOAD_FOLDER"))
-}
-
-func testRadarr() {
-	a := createAPI()
-	move := parser.DiskMove{}
-	_, err := parser.FixFailedShows(a, move)
+func execute(a api.API) {
+	parser.ExtractAll(a.DownloadFolder)
+	move := parser.DiskMover{}
+	files, err := parser.MoveFailedShows(a, move)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+	}
+	err = parser.CleanFixedShows(a, files)
+	if err != nil {
+		log.Println(err)
+	}
+	err = parser.Rename(a, files)
+	if err != nil {
+		log.Println(err)
 	}
 }
 
-func check() {
-	a := createAPI()
-	parser.ExtractAll(os.Getenv(api.EnvSonarrDownloadFolder))
-	shows, err := parser.FixFailedShows(a, parser.FakeMove{})
-	if err != nil {
-		log.Printf("error fixing shows: %s", err.Error())
-		return
+func getAPIs() (apis []api.API) {
+	if os.Getenv(api.EnvRadarrURL) != "" {
+		apis = append(apis, radarr())
 	}
-	if len(shows) == 0 {
-		log.Print("no failed episodes")
-		return
+	if os.Getenv(api.EnvSonarrURL) != "" {
+		apis = append(apis, sonarr())
 	}
-	err = parser.CleanFixedShows(a, shows)
-	if err != nil {
-		log.Printf("error cleaning shows: %s", err.Error())
-		return
-	}
+	return apis
 }
 
-func loadEnvOrFatal() {
-	godotenv.Load()
+func sonarr() api.API {
 	if os.Getenv(api.EnvSonarrAPIKey) == "" {
-		log.Fatal("empty apikey")
+		log.Fatal("empty sonarr apikey")
 	}
 	if os.Getenv(api.EnvSonarrDownloadFolder) == "" {
-		log.Fatal("empty download folder")
+		log.Fatal("empty sonarr download folder")
 	}
 	if os.Getenv(api.EnvSonarrURL) == "" {
 		log.Fatal("empty sonarr url")
 	}
+	return api.NewAPI(
+		os.Getenv("SONARR_URL"),
+		os.Getenv("SONARR_APIKEY"),
+		os.Getenv("SONARR_DOWNLOAD_FOLDER"))
+}
+
+func radarr() api.API {
+	if os.Getenv(api.EnvRadarrAPIKey) == "" {
+		log.Fatal("empty radarr apikey")
+	}
+	if os.Getenv(api.EnvRadarrDownloadFolder) == "" {
+		log.Fatal("empty radarr download folder")
+	}
+	if os.Getenv(api.EnvRadarrURL) == "" {
+		log.Fatal("empty radarr url")
+	}
+	return api.NewAPI(
+		os.Getenv("SONARR_URL"),
+		os.Getenv("SONARR_APIKEY"),
+		os.Getenv("SONARR_DOWNLOAD_FOLDER"))
 }
